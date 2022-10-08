6db3d8321defa49476f916580da6fd391920ecf4
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-10-08 21:47:02 +0800

    update

--- scripts/pressaction.lua
+++ scripts/pressaction.lua
@@ -1,128 +1,138 @@
-local opt = require("mp.options")
-local options = {
-    bind = "SPACE",
-    action = "no-osd set speed 4; set pause no",
-    invert = "",
-    duration = 200
-}
-opt.read_options(options)
+-- This script is deprecated
+-- Please use inputevent.lua instead
+-- https://github.com/Natural-Harmonia-Gropius/mpv_config/blob/main/scripts/inputevent.lua
+-- Add these lines to input.conf to restore the same behavior
+-- SPACE  cycle pause                       #@click
+-- SPACE  no-osd set speed 4; set pause no  #@press
+-- SPACE  ignore                            #@release
 
-local pressed = false
-local keydown_at = 0
-local original = ""
-local invert = ""
 
--- https://mpv.io/manual/master/#input-command-prefixes
-local prefixes = { "osd-auto", "no-osd", "osd-bar", "osd-msg", "osd-msg-bar", "raw", "expand-properties", "repeatable",
-    "async", "sync" }
 
--- https://mpv.io/manual/master/#list-of-input-commands
-local commands = { "set", "cycle", "add", "multiply" }
+-- local opt = require("mp.options")
+-- local options = {
+--     bind = "SPACE",
+--     action = "no-osd set speed 4; set pause no",
+--     invert = "",
+--     duration = 200
+-- }
+-- opt.read_options(options)
 
-function table:has(element)
-    for _, value in ipairs(self) do
-        if value == element then
-            return true
-        end
-    end
-    return false
-end
+-- local pressed = false
+-- local keydown_at = 0
+-- local original = ""
+-- local invert = ""
 
-function string:split(separator)
-    local fields = {}
-    local separator = separator or ":"
-    local pattern = string.format("([^%s]+)", separator)
-    local copy = self:gsub(pattern, function(c) fields[#fields + 1] = c end)
-    return fields
-end
+-- -- https://mpv.io/manual/master/#input-command-prefixes
+-- local prefixes = { "osd-auto", "no-osd", "osd-bar", "osd-msg", "osd-msg-bar", "raw", "expand-properties", "repeatable",
+--     "async", "sync" }
 
-function string:trim()
-    return (self:gsub("^%s*(.-)%s*$", "%1"))
-end
+-- -- https://mpv.io/manual/master/#list-of-input-commands
+-- local commands = { "set", "cycle", "add", "multiply" }
 
-function now()
-    return mp.get_time() * 1000
-end
+-- function table:has(element)
+--     for _, value in ipairs(self) do
+--         if value == element then
+--             return true
+--         end
+--     end
+--     return false
+-- end
 
-function command(command)
-    return mp.command(command)
-end
+-- function string:split(separator)
+--     local fields = {}
+--     local separator = separator or ":"
+--     local pattern = string.format("([^%s]+)", separator)
+--     local copy = self:gsub(pattern, function(c) fields[#fields + 1] = c end)
+--     return fields
+-- end
 
-function get_key_binding(key)
-    for _, v in ipairs(mp.get_property_native("input-bindings")) do
-        if v.key == key then
-            return v.cmd
-        end
-    end
-    return "ignore"
-end
+-- function string:trim()
+--     return (self:gsub("^%s*(.-)%s*$", "%1"))
+-- end
 
-function get_invert(action)
-    if options.invert ~= "" then
-        return options.invert
-    end
+-- function now()
+--     return mp.get_time() * 1000
+-- end
 
-    local invert = ""
-    local action = action:split(";")
-    for i, v in ipairs(action) do
-        local subs = v:trim():split("%s*")
-        local prefix = table.has(prefixes, subs[1]) and subs[1] or ""
-        local command = subs[prefix == "" and 1 or 2]
-        local property = subs[prefix == "" and 2 or 3]
-        local value = mp.get_property(property)
-        local semi = i == #action and "" or ";"
+-- function command(command)
+--     return mp.command(command)
+-- end
 
-        if table.has(commands, command) then
-            invert = invert .. prefix .. " " .. "set" .. " " .. property .. " " .. value .. semi
-        else
-            mp.msg.error(v:trim() .. "' doesn't support auto restore, please set 'options.invert' manually")
-        end
-    end
-    return invert
-end
+-- function get_key_binding(key)
+--     for _, v in ipairs(mp.get_property_native("input-bindings")) do
+--         if v.key == key then
+--             return v.cmd
+--         end
+--     end
+--     return "ignore"
+-- end
 
-function keydown(key_name, key_text, is_mouse)
-    keydown_at = now()
-    original = get_key_binding(options.bind)
-    invert = get_invert(options.action)
-end
+-- function get_invert(action)
+--     if options.invert ~= "" then
+--         return options.invert
+--     end
 
-function keyup(key_name, key_text, is_mouse)
-    command(pressed and invert or original)
-    pressed = false
-    keydown_at = 0
-end
+--     local invert = ""
+--     local action = action:split(";")
+--     for i, v in ipairs(action) do
+--         local subs = v:trim():split("%s*")
+--         local prefix = table.has(prefixes, subs[1]) and subs[1] or ""
+--         local command = subs[prefix == "" and 1 or 2]
+--         local property = subs[prefix == "" and 2 or 3]
+--         local value = mp.get_property(property)
+--         local semi = i == #action and "" or ";"
 
-function keypress(key_name, key_text, is_mouse)
-end
+--         if table.has(commands, command) then
+--             invert = invert .. prefix .. " " .. "set" .. " " .. property .. " " .. value .. semi
+--         else
+--             mp.msg.error(v:trim() .. "' doesn't support auto restore, please set 'options.invert' manually")
+--         end
+--     end
+--     return invert
+-- end
 
-function keyrepeat(key_name, key_text, is_mouse)
-    if pressed then
-        return
-    end
+-- function keydown(key_name, key_text, is_mouse)
+--     keydown_at = now()
+--     original = get_key_binding(options.bind)
+--     invert = get_invert(options.action)
+-- end
 
-    if now() - keydown_at < options.duration then
-        return
-    end
+-- function keyup(key_name, key_text, is_mouse)
+--     command(pressed and invert or original)
+--     pressed = false
+--     keydown_at = 0
+-- end
 
-    pressed = true
-    command(options.action)
-end
+-- function keypress(key_name, key_text, is_mouse)
+-- end
 
-function event_handler(event, is_mouse, key_name, key_text)
-    if event == "down" then
-        keydown(key_name, key_text, is_mouse)
-    elseif event == "up" then
-        keyup(key_name, key_text, is_mouse)
-    elseif event == "press" then
-        keypress(key_name, key_text, is_mouse)
-    elseif event == "repeat" then
-        keyrepeat(key_name, key_text, is_mouse)
-    else
-        print(event, key_name, key_text, is_mouse)
-    end
-end
+-- function keyrepeat(key_name, key_text, is_mouse)
+--     if pressed then
+--         return
+--     end
 
-mp.add_forced_key_binding(options.bind, nil, function(e)
-    event_handler(e.event, e.is_mouse, e.key_name, e.key_text)
-end, { complex = true })
+--     if now() - keydown_at < options.duration then
+--         return
+--     end
+
+--     pressed = true
+--     command(options.action)
+-- end
+
+-- function event_handler(event, is_mouse, key_name, key_text)
+--     if event == "down" then
+--         keydown(key_name, key_text, is_mouse)
+--     elseif event == "up" then
+--         keyup(key_name, key_text, is_mouse)
+--     elseif event == "press" then
+--         keypress(key_name, key_text, is_mouse)
+--     elseif event == "repeat" then
+--         keyrepeat(key_name, key_text, is_mouse)
+--     else
+--         print(event, key_name, key_text, is_mouse)
+--     end
+-- end
+
+-- mp.add_forced_key_binding(options.bind, nil, function(e)
+--     event_handler(e.event, e.is_mouse, e.key_name, e.key_text)
+-- end, { complex = true })
