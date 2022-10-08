a7d5ed8aae4b1078309229fa53b987a7ac1612af
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-10-08 14:20:54 +0800

    update

---
+++ scripts/on.lua
@@ -0,0 +1,105 @@
+local function debounce(func, wait)
+    func = type(func) == "function" and func or function() end
+    wait = type(wait) == "number" and wait / 1000 or 0
+
+    local timer = nil
+    local timer_end = function()
+        timer:kill()
+        timer = nil
+        func()
+    end
+
+    return function()
+        if timer then
+            timer:kill()
+        end
+        timer = mp.add_timeout(wait, timer_end)
+    end
+end
+
+function now()
+    return mp.get_time() * 1000
+end
+
+function command(command)
+    return mp.command(command)
+end
+
+function table:append(source)
+    self[#self + 1] = source
+    return self
+end
+
+function table:assign(source)
+    for key, value in pairs(source) do
+        self[key] = value
+    end
+    return self
+end
+
+local target = {
+    name = "",
+    key = "MBTN_LEFT",
+    click = "cycle pause",
+    double_click = "cycle fullscreen",
+    press = "show-text pressed",
+    release = "show-text released",
+    duration = 200,
+    queue = {},
+}
+
+function target:handler(e)
+    local queue_item = {
+        event = e.event,
+        at = now()
+    }
+    self.queue = table.append(self.queue, queue_item)
+    self.exec()
+end
+
+function target:bind()
+    self.name = self.name or ("@" .. self.key)
+    self.exec = debounce(function()
+        function dump(o)
+            if type(o) == 'table' then
+                local s = '{ '
+                for k, v in pairs(o) do
+                    if type(k) ~= 'number' then k = '"' .. k .. '"' end
+                    s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+                end
+                return s .. '} '
+            end
+            return tostring(o)
+        end
+
+        local queue = self.queue
+        local length = #queue
+        local last = queue[length]
+        local lantency = now() - last.at
+        local str = dump(queue)
+
+        print("lantency:", lantency, "length:", length, "toString:", str)
+
+        if length == 1 then
+            if last.event == "down" then
+                command(self.press)
+            elseif last.event == "up" then
+                command(self.release)
+            end
+        elseif length == 2 then
+            command(self.click)
+        elseif length == 4 then
+            command(self.double_click)
+        end
+
+        self.queue = {}
+    end, self.duration)
+
+    mp.add_forced_key_binding(self.key, self.name, function(e) self:handler(e) end, { complex = true })
+end
+
+function target:unbind()
+    mp.remove_key_binding(self.name)
+end
+
+target:bind()
