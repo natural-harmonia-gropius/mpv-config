0e976aaba94ba90432ef0e05e7882e65c72bcfd8
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-11-17 08:47:02 +0800

    update

--- portable_config/shaders/hdr-toys/tone-mapping/piecewise.glsl
+++ portable_config/shaders/hdr-toys/tone-mapping/piecewise.glsl
@@ -5,6 +5,39 @@
 //!BIND HOOKED
 //!DESC tone mapping (piecewise)
 
+vec3 RGB_to_XYZ(float R, float G, float B) {
+    mat3 M = mat3(
+        0.6370, 0.1446, 0.1689,
+        0.2627, 0.6780, 0.0593,
+        0.0000, 0.0281, 1.0610);
+    return M * vec3(R, G, B);
+}
+
+vec3 XYZ_to_RGB(float X, float Y, float Z) {
+    mat3 M = mat3(
+         1.7167, -0.3557, -0.2534,
+        -0.6667,  1.6165,  0.0158,
+         0.0176, -0.0428,  0.9421);
+    return M * vec3(X, Y, Z);
+}
+
+vec3 XYZ_to_xyY(float X, float Y, float Z) {
+    float divisor = X + Y + Z;
+    if (divisor == 0.0) divisor = 1e-6;
+
+    float x = X / divisor;
+    float y = Y / divisor;
+
+    return vec3(x, y, Y);
+}
+
+vec3 xyY_to_XYZ(float x, float y, float Y) {
+    float X = x * Y / max(y, 1e-6);
+    float Z = (1.0 - x - y) * Y / max(y, 1e-6);
+
+    return vec3(X, Y, Z);
+}
+
 vec2 asSlopeIntercept(float x0, float x1, float y0, float y1) {
     float dy = (y1 - y0);
     float dx = (x1 - x0);
@@ -31,8 +64,8 @@ float evalCurveSegment(float x, float offsetX, float offsetY, float scaleX, floa
 
 // User Params
 const float toeStrength = 0.1;  // [0-1]
-const float toeLength = 0.5;  // [0-1]
-const float shoulderStrength = 2.5;  // in F stops [0-10]
+const float toeLength = 0.1;  // [0-1]
+const float shoulderStrength = 3.3;  // in F stops [0-10]
 const float shoulderLength = 0.5;  // [0-1]
 const float shoulderAngle = 1.0;  // [0-1]
 
@@ -141,7 +174,10 @@ float curve(float x) {
 
 vec4 color = HOOKED_tex(HOOKED_pos);
 vec4 hook() {
-    const float L = dot(color.rgb, vec3(0.2627, 0.6780, 0.0593));
-    color.rgb *= curve(L) / L;
+    color.rgb = RGB_to_XYZ(color.r, color.g, color.b);
+    color.rgb = XYZ_to_xyY(color.r, color.g, color.b);
+    color.z  *= curve(color.z) / color.z;
+    color.rgb = xyY_to_XYZ(color.r, color.g, color.b);
+    color.rgb = XYZ_to_RGB(color.r, color.g, color.b);
     return color;
 }
