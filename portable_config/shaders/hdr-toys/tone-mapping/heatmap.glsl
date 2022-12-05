3bc0841306583a952d1098bfb8aaabac0f349637
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-12-06 00:32:33 +0800

    update

--- portable_config/shaders/hdr-toys/tone-mapping/heatmap.glsl
+++ portable_config/shaders/hdr-toys/tone-mapping/heatmap.glsl
@@ -14,26 +14,29 @@ vec4 color = HOOKED_tex(HOOKED_pos);
 vec4 hook() {
     const float L = dot(color.rgb, vec3(0.2627, 0.6780, 0.0593));
 
-    const float l1 = 1;
-    const float l2 = 1000  / WHITE_sdr;
-    const float l3 = 2000  / WHITE_sdr;
-    const float l4 = 4000  / WHITE_sdr;
-    const float l5 = 10000 / WHITE_sdr;
+    const float l0 = 0.1 / WHITE_sdr;
+    const float l1 = 1.0;
+    const float l2 = 1000.0  / WHITE_sdr;
+    const float l3 = 2000.0  / WHITE_sdr;
+    const float l4 = 4000.0  / WHITE_sdr;
+    const float l5 = 10000.0 / WHITE_sdr;
 
     if (L > l5) {
-        color.rgb = vec3(1, 0, 0.6);
+        color.rgb = vec3(1.0, 0.0, 0.6);
     } else if (L > l4) {
         const float a = (L - l4) / (l5 - l4);
-        color.rgb = vec3(1, 1, a);
+        color.rgb = vec3(1.0, 1.0, a);
     } else if (L > l3) {
         const float a = (L - l3) / (l4 - l3);
-        color.rgb = vec3(a, 0, 0);
+        color.rgb = vec3(a, 0.0, 0.0);
     } else if (L > l2) {
         const float a = (L - l2) / (l3 - l2);
-        color.rgb = vec3(0, a, 0);
+        color.rgb = vec3(0.0, a, 0.0);
     } else if (L > l1) {
         const float a = (L - l1) / (l2 - l1);
-        color.rgb = vec3(0, 0, a);
+        color.rgb = vec3(0.0, 0.0, a);
+    } else if (L < l0) {
+        color.rgb = vec3(0.0, 0.0, 0.0);
     } else {
         color.rgb = vec3(L, L, L);
     }
