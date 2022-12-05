ea49874faf361da72647ebe6761e7aedd96cba9c
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-12-06 00:09:39 +0800

    update

---
+++ portable_config/shaders/hdr-toys/tone-mapping/heatmap.glsl
@@ -0,0 +1,41 @@
+// Heatmap of luminance
+
+//!PARAM WHITE_sdr
+//!TYPE float
+//!MINIMUM 0
+//!MAXIMUM 1000
+203.0
+
+//!HOOK OUTPUT
+//!BIND HOOKED
+//!DESC tone mapping (heatmap)
+
+vec4 color = HOOKED_tex(HOOKED_pos);
+vec4 hook() {
+    const float L = dot(color.rgb, vec3(0.2627, 0.6780, 0.0593));
+
+    const float l1 = 1;
+    const float l2 = 1000  / WHITE_sdr;
+    const float l3 = 2000  / WHITE_sdr;
+    const float l4 = 4000  / WHITE_sdr;
+    const float l5 = 10000 / WHITE_sdr;
+
+    if (L > l5) {
+        color.rgb = vec3(1, 0, 0.6);
+    } else if (L > l4) {
+        const float a = (L - l4) / (l5 - l4);
+        color.rgb = vec3(1, 1, a);
+    } else if (L > l3) {
+        const float a = (L - l3) / (l4 - l3);
+        color.rgb = vec3(a, 0, 0);
+    } else if (L > l2) {
+        const float a = (L - l2) / (l3 - l2);
+        color.rgb = vec3(0, a, 0);
+    } else if (L > l1) {
+        const float a = (L - l1) / (l2 - l1);
+        color.rgb = vec3(0, 0, a);
+    } else {
+        color.rgb = vec3(L, L, L);
+    }
+    return color;
+}
