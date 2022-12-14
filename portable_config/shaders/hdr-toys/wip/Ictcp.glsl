0777735ed3cbbac8004c7639005aa3dc66e5fe0c
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-12-14 15:01:20 +0800

    update

--- portable_config/shaders/hdr-toys/wip/Ictcp.glsl
+++ portable_config/shaders/hdr-toys/wip/Ictcp.glsl
@@ -1,6 +1,37 @@
+//!PARAM L_sdr
+//!TYPE float
+//!MINIMUM 0
+//!MAXIMUM 1000
+203.0
+
 //!HOOK OUTPUT
 //!BIND HOOKED
 //!DESC Ictcp
+
+const float pq_m1 = 0.1593017578125;
+const float pq_m2 = 78.84375;
+const float pq_c1 = 0.8359375;
+const float pq_c2 = 18.8515625;
+const float pq_c3 = 18.6875;
+
+const float pq_C  = 10000.0;
+
+float Y_2_ST2084(float C) {
+    float L = C / pq_C;
+    float Lm = pow(L, pq_m1);
+    float N = (pq_c1 + pq_c2 * Lm) / (1.0 + pq_c3 * Lm);
+    N = pow(N, pq_m2 );
+    return N;
+}
+
+float ST2084_2_Y(float N) {
+    float Np = pow(N, 1.0 / pq_m2);
+    float L = Np - pq_c1;
+    if (L < 0.0 ) L = 0.0;
+    L = L / (pq_c2 - pq_c3 * Np);
+    L = pow(L, 1.0 / pq_m1);
+    return L * pq_C;
+}
 
 vec3 RGB_to_XYZ(float R, float G, float B) {
     mat3 M = mat3(
@@ -26,36 +57,48 @@ vec3 XYZ_to_LMS(float X, float Y, float Z) {
     return vec3(X, Y, Z) * M;
 }
 
-const float c1  =  3424 / 4096;
-const float c2  =  2413 / 128;
-const float c3  =  2392 / 128;
-const float m1  =  2610 / 16384;
-const float m2  =  2523 / 32;
-const float im1 = 16384 / 2610;
-const float im2 =    32 / 2523;
-
-float PQEOTF(float val) {
-    float num = c1 + (c2 * pow((val / 10000), m1));
-    float den = 1 + (c3 * pow((val / 10000), m1));
-    return pow(num / den, m2);
+vec3 LMS_to_XYZ(float L, float M, float S) {
+    mat3 MM = mat3(
+         2.071, -1.327,  0.207,
+         0.365,  0.681, -0.045,
+        -0.049, -0.050,  1.188);
+    return vec3(L, M, S) * MM;
 }
 
-vec3 LMS_to_IPT(float L, float M, float S) {
+vec3 LMS_to_ICtCp(float L, float M, float S) {
     vec3 VV = vec3(L, M, S);
-    VV.r = PQEOTF(VV.r);
-    VV.g = PQEOTF(VV.g);
-    VV.b = PQEOTF(VV.b);
+    VV.r = Y_2_ST2084(VV.r);
+    VV.g = Y_2_ST2084(VV.g);
+    VV.b = Y_2_ST2084(VV.b);
     mat3 MM = mat3(
-         2048 / 4096,   2048 / 4096,      0     ,
-         6610 / 4096, -13613 / 4096, 7003 / 4096,
-        17933 / 4096, -17390 / 4096, -543 / 4096);
+         2048,   2048,    0,
+         6610, -13613, 7003,
+        17933, -17390, -543) / 4096;
     return VV * MM;
+}
+
+vec3 ICtCp_to_LMS(float I, float Ct, float Cp) {
+    vec3 VV = vec3(I, Ct, Cp);
+    mat3 MM = mat3(
+        1.0,  0.009,  0.111,
+        1.0, -0.009, -0.111,
+        1.0,  0.560, -0.321);
+    VV *= MM;
+    VV.r = ST2084_2_Y(VV.r);
+    VV.g = ST2084_2_Y(VV.g);
+    VV.b = ST2084_2_Y(VV.b);
+    return VV;
 }
 
 vec4 color = HOOKED_tex(HOOKED_pos);
 vec4 hook() {
+    color.rgb *= L_sdr;
     color.rgb = RGB_to_XYZ(color.r, color.g, color.b);
     color.rgb = XYZ_to_LMS(color.r, color.g, color.b);
-    color.rgb = LMS_to_IPT(color.r, color.g, color.b);
+    color.rgb = LMS_to_ICtCp(color.r, color.g, color.b);
+    color.rgb = ICtCp_to_LMS(color.r, color.g, color.b);
+    color.rgb = LMS_to_XYZ(color.r, color.g, color.b);
+    color.rgb = XYZ_to_RGB(color.r, color.g, color.b);
+    color.rgb /= L_sdr;
     return color;
 }
