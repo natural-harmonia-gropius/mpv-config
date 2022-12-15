c209a8cb389da7a10d0d177554ee911ed67f4c3e
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-12-16 02:01:20 +0800

    update

--- portable_config/shaders/hdr-toys/tone-mapping/hable2.glsl
+++ portable_config/shaders/hdr-toys/tone-mapping/hable2.glsl
@@ -13,41 +13,50 @@
 //!MAXIMUM 1000
 203.0
 
-//!PARAM toeStrength
-//!TYPE float
-//!MINIMUM 0.0
-//!MAXIMUM 1.0
-0.5
-
-//!PARAM toeLength
-//!TYPE float
-//!MINIMUM 0.0
-//!MAXIMUM 1.0
-0.1
-
-//!PARAM shoulderLength
-//!TYPE float
-//!MINIMUM 0.0
-//!MAXIMUM 1.0
-0.5
-
-//!PARAM shoulderAngle
-//!TYPE float
-//!MINIMUM 0.0
-//!MAXIMUM 1.0
-1.0
-
-//!PARAM gamma
-//!TYPE float
-//!MINIMUM 1.0
-//!MAXIMUM 3.0
-1.0
-
 //!HOOK OUTPUT
 //!BIND HOOKED
 //!DESC tone mapping (hable2)
 
-vec2 asSlopeIntercept(float x0, float x1, float y0, float y1) {
+float curve_segment_eval(float x, float lnA, float B, float offsetX, float offsetY, float scaleX, float scaleY) {
+    float x0 = (x - offsetX) * scaleX;
+	float y0 = 0.0;
+
+    // log(0) is undefined but our function should evaluate to 0. There are better ways to handle this,
+	// but it's doing it the slow way here for clarity.
+	if (x0 > 0.0) {
+		y0 = exp(lnA + B * log(x0));
+	}
+
+    return y0 * scaleY + offsetY;
+}
+
+float curve_segment_eval_inv(float y, float lnA, float B, float offsetX, float offsetY, float scaleX, float scaleY) {
+	float y0 = (y - offsetY) / scaleY;
+	float x0 = 0.0;
+
+	// watch out for log(0) again
+	if (y0 > 0.0) {
+		x0 = exp((log(y0) - lnA) / B);
+	}
+	float x = x0 / scaleX + offsetX;
+
+	return x;
+}
+
+// find a function of the form:
+//   f(x) = e^(lnA + Bln(x))
+// where
+//   f(0)   = 0; not really a constraint
+//   f(x0)  = y0
+//   f'(x0) = m
+vec2 solve_AB(float x0, float y0, float m) {
+    float B   = (m * x0) / y0;
+    float lnA = log(y0) - B * log(x0);
+    return vec2(lnA, B);
+}
+
+// convert to y=mx+b
+vec2 as_slope_intercept(float x0, float x1, float y0, float y1) {
     float dy = (y1 - y0);
     float dx = (x1 - x0);
     float m  = dx == 0.0 ? 1.0 : dy / dx;
@@ -55,64 +64,87 @@ vec2 asSlopeIntercept(float x0, float x1, float y0, float y1) {
     return vec2(m, b);
 }
 
-float evalDerivativeLinearGamma(float m, float b, float g, float x) {
+// f(x) = (mx+b)^g
+// f'(x) = gm(mx+b)^(g-1)
+float eval_derivative_linear_gamma(float m, float b, float g, float x) {
     return g * m * pow(m * x + b, g - 1.0);
 }
 
-vec2 solveAB(float x0, float y0, float m) {
-    float B   = (m * x0) / y0;
-    float lnA = log(y0) - B * log(x0);
-    return vec2(lnA, B);
-}
-
-float evalCurveSegment(float x, float offsetX, float offsetY, float scaleX, float scaleY, float lnA, float B) {
-    float x0 = (x - offsetX) * scaleX;
-    float y0 = x0 > 0.0 ? exp(lnA + B * log(x0)) : 0.0;
-    return y0 * scaleY + offsetY;
-}
-
 float curve(float x) {
-    const float w = L_hdr / L_sdr;
-    const float shoulderStrength = clamp(log2(w), 0.0, 10.0);
+    float toeLength = 0.1;
+    float toeStrength = 0.5;
+    float shoulderAngle = 1.0;
+    float shoulderLength = 0.5;
+    float shoulderStrength = log2(L_hdr / L_sdr);
+
+    float x0 = 0.0;
+    float y0 = 0.0;
+    float x1 = 0.0;
+    float y1 = 0.0;
+    float W  = 0.0;
+    float overshootX = 0.0;
+    float overshootY = 0.0;
 
     // Convert from "user" to "direct" parameters
 
-    // This is not actually the display gamma. It's just a UI space to avoid having
-    // to enter small numbers for the input.
-    // As a simple trick, we can apply a power of 2.2 to toeLength to allow finer
-    // control of the toe.
-    const float perceptualGamma = 2.4;
+    // This is not actually the display gamma. It's just a UI space to avoid having to
+    // enter small numbers for the input.
+    float perceptualGamma = 2.4;
 
-    float x0 = pow(toeLength, perceptualGamma) * 0.5;  // toe goes from 0 to 0.5
-    float y0 = (1.0 - toeStrength ) * x0; // lerp from 0 to x0
+    // constraints
+    toeLength = pow(clamp(toeLength, 0.0, 1.0), perceptualGamma);
+    toeStrength = clamp(toeStrength, 0.0, 1.0);
+    shoulderAngle = clamp(shoulderAngle, 0.0, 1.0);
+    shoulderLength = max(1e-5, clamp(shoulderLength, 0.0, 1.0));
+    shoulderStrength = max(0.0, shoulderStrength);
 
-    const float remainingY = 1.0 - y0;
+    // apply base params
+    x0 = toeLength * 0.5; // toe goes from 0 to 0.5
+    y0 = (1.0 - toeStrength) * x0; // lerp from 0 to x0
 
-    const float y1_offset = (1.0 - shoulderLength) * remainingY;
-    float x1 = x0 + y1_offset;
-    float y1 = y0 + y1_offset;
+    float remainingY = 1.0 - y0;
 
-    const float initialW = x0 + remainingY;
-    const float extraW = pow(2.0, shoulderStrength) - 1.0;
-    const float W = initialW + extraW;
+    float initialW = x0 + remainingY;
 
-    float overshootX = (2.0 * W) * shoulderAngle * shoulderStrength / W;
-    float overshootY = 0.5 * shoulderAngle * shoulderStrength;
+    float y1_offset = (1.0 - shoulderLength) * remainingY;
+    x1 = x0 + y1_offset;
+    y1 = y0 + y1_offset;
 
+    // filmic shoulder strength is in F stops
+    float extraW = exp2(shoulderStrength) - 1.0;
+
+    W = initialW + extraW;
+
+    overshootX = (W * 2.0) * shoulderAngle * shoulderStrength;
+    overshootY = 0.5 * shoulderAngle * shoulderStrength;
+
+    // CreateCurve
+
+	// normalize params to 1.0 range
+    float invW = 1.0 / W;
     x0 /= W;
     x1 /= W;
+    overshootX /= W;
+    W = 1.0;
 
     // Precompute information for all three segments (mid, toe, shoulder)
-    const vec2  tmp = asSlopeIntercept(x0, x1, y0, y1);
+    const vec2  tmp = as_slope_intercept(x0, x1, y0, y1);
     const float m = tmp.x,
                 b = tmp.y,
-                g = 1.0 / gamma;
+                g = 1.0; // gamma
 
-    if (g != 1.0) {
-        y0 = max(pow(y0, g), 1e-6);
-        y1 = max(pow(y1, g), 1e-6);
-        overshootY = pow(1.0 + overshootY, g) - 1.0;
-    }
+    // base function of linear section plus gamma is
+    // y = (mx+b)^g
+
+    // which we can rewrite as
+    // y = exp(g*ln(m) + g*ln(x+b/m))
+
+    // and our evaluation function is (skipping the if parts):
+    /*
+        float x0 = (x - m_offsetX)*m_scaleX;
+        y0 = expf(m_lnA + m_B*logf(x0));
+        return y0*m_scaleY + m_offsetY;
+    */
 
     float   midOffsetX  = -(b / m),
             midOffsetY  = 0.0,
@@ -121,8 +153,14 @@ float curve(float x) {
             midLnA      = g * log(m),
             midB        = g;
 
-    const float toeM = evalDerivativeLinearGamma(m, b, g, x0);
-    const vec2  toeAB   = solveAB(x0, y0, m);
+    const float toeM = eval_derivative_linear_gamma(m, b, g, x0);
+    const float shoulderM = eval_derivative_linear_gamma(m, b, g, x1);
+
+    y0 = max(pow(y0, g), 1e-6);
+    y1 = max(pow(y1, g), 1e-6);
+    overshootY = pow(1.0 + overshootY, g) - 1.0;
+
+    const vec2  toeAB   = solve_AB(x0, y0, m);
     float   toeOffsetX  = 0.0,
             toeOffsetY  = 0.0,
             toeScaleX   = 1.0,
@@ -132,8 +170,8 @@ float curve(float x) {
 
     const float shoulderX0  = (1.0 + overshootX) - x1;
     const float shoulderY0  = (1.0 + overshootY) - y1;
-    const float shoulderM   = evalDerivativeLinearGamma(m, b, g, x1);
-    const vec2  shoulderAB  = solveAB(shoulderX0, shoulderY0, m);
+
+    const vec2  shoulderAB  = solve_AB(shoulderX0, shoulderY0, m);
     float   shoulderOffsetX = 1.0 + overshootX,
             shoulderOffsetY = 1.0 + overshootY,
             shoulderScaleX  = -1.0,
@@ -142,37 +180,39 @@ float curve(float x) {
             shoulderB       = shoulderAB.y;
 
     // Normalize (correct for overshooting)
-    const float scale = 1.0 / evalCurveSegment(1.0,
+    const float scale = curve_segment_eval(1.0,
+        shoulderLnA, shoulderB,
         shoulderOffsetX, shoulderOffsetY,
-        shoulderScaleX, shoulderScaleY,
-        shoulderLnA, shoulderB);
-    toeOffsetY      *= scale;
-    toeScaleY       *= scale;
-    midOffsetY      *= scale;
-    midScaleY       *= scale;
-    shoulderOffsetY *= scale;
-    shoulderScaleY  *= scale;
+        shoulderScaleX, shoulderScaleY);
+    const float invScale = 1.0 / scale;
 
-    const float Xn = x / W;
-    float result;
-    if (Xn < x0) {
-        result = evalCurveSegment(Xn,
+    toeOffsetY *= invScale;
+    toeScaleY  *= invScale;
+
+    midOffsetY *= invScale;
+    midScaleY  *= invScale;
+
+    shoulderOffsetY *= invScale;
+    shoulderScaleY  *= invScale;
+
+    // FullCurve::Eval
+    float normX = x * invW;
+    if (normX < x0) {
+        return curve_segment_eval(normX,
+            toeLnA, toeB,
             toeOffsetX, toeOffsetY,
-            toeScaleX, toeScaleY,
-            toeLnA, toeB);
-    } else if (Xn < x1) {
-        result = evalCurveSegment(Xn,
+            toeScaleX, toeScaleY);
+    } else if (normX < x1) {
+        return curve_segment_eval(normX,
+            midLnA, midB,
             midOffsetX, midOffsetY,
-            midScaleX, midScaleY,
-            midLnA, midB);
+            midScaleX, midScaleY);
     } else {
-        result = evalCurveSegment(Xn,
+        return curve_segment_eval(normX,
+            shoulderLnA, shoulderB,
             shoulderOffsetX, shoulderOffsetY,
-            shoulderScaleX, shoulderScaleY,
-            shoulderLnA, shoulderB);
+            shoulderScaleX, shoulderScaleY);
     }
-
-    return result;
 }
 
 vec4 color = HOOKED_tex(HOOKED_pos);
