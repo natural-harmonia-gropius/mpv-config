227d8303f145392a1ce9d08a31777caeafce9674
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-12-19 19:53:54 +0800

    update

--- portable_config/shaders/hdr-toys/tone-mapping/clip.glsl
+++ portable_config/shaders/hdr-toys/tone-mapping/clip.glsl
@@ -10,19 +10,37 @@
 //!BIND HOOKED
 //!DESC tone mapping (clip)
 
-float curve(float x) {
-    const float WHITE = 1.0;
-    const float BLACK = WHITE / CONTRAST_sdr;
+const float DISPGAMMA = 2.4;
+const float L_W = 1.0;
+const float L_B = 0.0;
 
-    x = (x - BLACK) / (WHITE - BLACK);
-    x = clamp(x, 0.0, WHITE);
-    return x;
+float bt1886_r(float L, float gamma, float Lw, float Lb) {
+    float a = pow(pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma), gamma);
+    float b = pow(Lb, 1.0 / gamma) / (pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma));
+    float V = pow(max(L / a, 0.0), 1.0 / gamma) - b;
+    return V;
+}
+
+float bt1886_f(float V, float gamma, float Lw, float Lb) {
+    float a = pow(pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma), gamma);
+    float b = pow(Lb, 1.0 / gamma) / (pow(Lw, 1.0 / gamma) - pow(Lb, 1.0 / gamma));
+    float L = a * pow(max(V + b, 0.0), gamma);
+    return L;
 }
 
 vec4 color = HOOKED_tex(HOOKED_pos);
 vec4 hook() {
-    color.r = curve(color.r);
-    color.g = curve(color.g);
-    color.b = curve(color.b);
+    color.rgb = vec3(
+        bt1886_r(color.r, DISPGAMMA, L_W, L_W / CONTRAST_sdr),
+        bt1886_r(color.g, DISPGAMMA, L_W, L_W / CONTRAST_sdr),
+        bt1886_r(color.b, DISPGAMMA, L_W, L_W / CONTRAST_sdr)
+    );
+
+    color.rgb = vec3(
+        bt1886_f(color.r, DISPGAMMA, L_W, L_B),
+        bt1886_f(color.g, DISPGAMMA, L_W, L_B),
+        bt1886_f(color.b, DISPGAMMA, L_W, L_B)
+    );
+
     return color;
 }
