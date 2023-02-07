fd8fb797985ab6f2288dc2eddefdad2973fcfbd3
Natural-Harmonia-Gropius <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2023-02-08 07:51:19 +0800

    update

--- portable_config/shaders/hdr-toys/tone-mapping/local.glsl
+++ portable_config/shaders/hdr-toys/tone-mapping/local.glsl
@@ -1,35 +1,17 @@
-//!PARAM L_hdr
-//!TYPE float
-//!MINIMUM 0
-//!MAXIMUM 10000
-1000.0
-
-//!PARAM L_sdr
-//!TYPE float
-//!MINIMUM 0
-//!MAXIMUM 1000
-203.0
-
-//!PARAM CONTRAST_sdr
-//!TYPE float
-//!MINIMUM 0
-//!MAXIMUM 1000000
-1000.0
-
 //!HOOK OUTPUT
 //!BIND HOOKED
 //!COMPUTE 32 32
 //!DESC tone mapping (local)
 
-shared int L_max;
+shared float L_max;
 
 void metering() {
     ivec2 base = ivec2(gl_WorkGroupID) * ivec2(gl_WorkGroupSize);
     for (uint y = gl_LocalInvocationID.y; y < gl_WorkGroupSize.y; y++) {
         for (uint x = gl_LocalInvocationID.x; x < gl_WorkGroupSize.x; x++) {
             vec4 texelValue = texelFetch(HOOKED_raw, base + ivec2(x,y), 0);
-            float L = L_sdr * dot(texelValue.rgb, vec3(0.2627, 0.6780, 0.0593));
-            L_max = max(L_max, int(L));
+            float L = dot(texelValue.rgb, vec3(0.2627, 0.6780, 0.0593));
+            L_max = max(L_max, L);
         }
     }
 }
@@ -40,16 +22,16 @@ float curve(float x, float w) {
     return extended;
 }
 
+vec4 color = HOOKED_tex(HOOKED_pos);
 void hook() {
-    L_max = int(L_sdr);
+    L_max = 1.0;
 
     metering();
 
     barrier();
 
-    vec4 color = HOOKED_tex(HOOKED_pos);
     float L = dot(color.rgb, vec3(0.2627, 0.6780, 0.0593));
-    color.rgb *= curve(L, L_max / L_sdr) / L;
+    color.rgb *= curve(L, L_max) / L;
 
     imageStore(out_image, ivec2(gl_GlobalInvocationID), color);
 }
