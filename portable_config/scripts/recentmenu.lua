b8106cc65a42a92675e509c5b8f2fa5ed75dd154
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2023-02-14 17:05:21 +0800

    update

--- portable_config/scripts/recentmenu.lua
+++ portable_config/scripts/recentmenu.lua
@@ -2,15 +2,16 @@ local utils = require("mp.utils")
 local options = require("mp.options")
 
 local o = {
+    path = "~~/recent.json",
     length = 10,
 }
 options.read_options(o)
 
-local path = mp.command_native({ "expand-path", "~~/recent.json" })
+local path = mp.command_native({ "expand-path", o.path })
 
 local menu = {
     type = 'recent_menu',
-    title = 'Recent',
+    title = 'Recently played',
     items = { { title = 'Nothing here', value = 'ignore' } },
 }
 
@@ -37,9 +38,9 @@ function write_json()
     json_file:close()
 end
 
-function append_item(title, path)
+function append_item(path, filename, title)
     local new_items = {}
-    new_items[1] = { title = title, value = { "loadfile", path } }
+    new_items[1] = { title = filename, hint = title, value = { "loadfile", path } }
     for index, value in ipairs(menu.items) do
         if #new_items < o.length and value.value ~= "ignore" and value.value[2] ~= path then
             new_items[#new_items + 1] = value
@@ -55,11 +56,21 @@ function open_menu()
     mp.commandv('script-message-to', 'uosc', 'open-menu', json)
 end
 
+function get_filename(filename)
+    local idx = filename:match(".+()%.%w+$") --获取文件后缀
+    if idx then
+        filename = filename:sub(1, idx - 1)
+    end
+    return filename
+end
+
 function on_load()
     local path = mp.get_property("path")
     if not path then return end
+    local filename = get_filename(mp.get_property("filename"))
     local title = mp.get_property("media-title") or path
-    append_item(title, path)
+    if filename == title then title = "" end
+    append_item(path, filename, title)
 end
 
 mp.add_key_binding(nil, "open", open_menu)
