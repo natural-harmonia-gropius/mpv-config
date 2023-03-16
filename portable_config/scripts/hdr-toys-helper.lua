ab53df96c67cf653153a0f8d9998cfcd4211bcc0
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2023-03-17 01:11:29 +0800

    update

--- portable_config/scripts/hdr-toys-helper.lua
+++ portable_config/scripts/hdr-toys-helper.lua
@@ -1,30 +1,50 @@
-function math.clamp(x, min, max)
+local options = require("mp.options")
+
+local o = {
+    temporal_stable_time = 1 / 3,
+}
+options.read_options(o)
+
+function clamp(x, min, max)
     if x < min then return min end
     if x > max then return max end
     return x
 end
 
-function math.round(x)
+function round(x)
     return math.floor(x + 0.5)
 end
 
-function set_hdr_white(w)
-    w = math.clamp(w, 0, 10000)
-    mp.command("no-osd set glsl-shader-opts L_hdr=" .. w)
+function set_L_hdr(v)
+    v = clamp(v, 0, 10000)
+    v = round(v)
+    mp.command("no-osd set glsl-shader-opts L_hdr=" .. v)
+end
+
+function set_temporal_stable_frames(v)
+    v = clamp(v, 0, 120)
+    v = round(v)
+    mp.command("no-osd set glsl-shader-opts temporal_stable_frames=" .. v)
 end
 
 mp.observe_property("video-out-params", "native", function(_, value)
-    if not value then
+    if not value then return end
+
+    local sig_peak = value["sig-peak"];
+    if not sig_peak or sig_peak == 1 then
+        set_L_hdr(1000)
         return
     end
 
-    local peak = value["sig-peak"];
-    if not peak or peak == 1 then
-        set_hdr_white(1000)
-        return
-    end
+    mp.add_timeout(0.1, function()
+        set_L_hdr(sig_peak * 203)
+    end)
+end)
 
-    mp.add_timeout(0.1, function ()
-        set_hdr_white(math.round(203 * peak))
+mp.observe_property("container-fps", "native", function(_, fps)
+    if not fps then return end
+
+    mp.add_timeout(0.1, function()
+        set_temporal_stable_frames(fps * o.temporal_stable_time)
     end)
 end)
