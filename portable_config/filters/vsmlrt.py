c36e8430e0ac39d9a8f94a0a0879ee52d9cb4d81
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2025-05-29 16:11:31 +0000

    Apply automatic changes

--- portable_config/filters/vsmlrt.py
+++ portable_config/filters/vsmlrt.py
@@ -1,4 +1,4 @@
-__version__ = "3.22.14"
+__version__ = "3.22.16"
 
 __all__ = [
     "Backend", "BackendV2",
@@ -523,7 +523,7 @@ def DPIR(
         if strength.num_frames != clip.num_frames:
             raise ValueError(f'{func_name}: "strength" must be of the same length as "clip"')
 
-        strength = core.std.Expr(strength, "x 255 /", format=gray_format)
+        strength = _expr(strength, "x 255 /", format=gray_format)
     else:
         try:
             strength = float(strength)
@@ -824,7 +824,7 @@ def CUGAN(
 
         if backend.supports_onnx_serialization:
             if conformance and version == 2:
-                clip = core.std.Expr(clip, "x 0.7 * 0.15 +")
+                clip = _expr(clip, "x 0.7 * 0.15 +")
 
             clip = inference_with_fallback(
                 clips=[clip], network_path=model.SerializeToString(),
@@ -833,7 +833,7 @@ def CUGAN(
             )
 
             if conformance and version == 2:
-                clip = core.std.Expr(clip, "x 0.15 - 0.7 /")
+                clip = _expr(clip, "x 0.15 - 0.7 /")
 
             return clip
 
@@ -842,7 +842,7 @@ def CUGAN(
 
     # https://github.com/bilibili/ailab/blob/e102bef22384c629f82552dbec3d6b5bab125639/Real-CUGAN/upcunet_v3.py#L1275-L1276
     if conformance and version == 2:
-        clip = core.std.Expr(clip, "x 0.7 * 0.15 +")
+        clip = _expr(clip, "x 0.7 * 0.15 +")
 
     clip = inference_with_fallback(
         clips=[clip], network_path=network_path,
@@ -852,7 +852,7 @@ def CUGAN(
 
     # https://github.com/bilibili/ailab/blob/e102bef22384c629f82552dbec3d6b5bab125639/Real-CUGAN/upcunet_v3.py#L269
     if conformance and version == 2:
-        clip = core.std.Expr(clip, "x 0.15 - 0.7 /")
+        clip = _expr(clip, "x 0.15 - 0.7 /")
 
     return clip
 
@@ -1797,10 +1797,7 @@ def ArtCNN(
     )
 
     if model in (4, 5, 9):
-        if clip.format.bits_per_sample == 16:
-            clip = core.akarin.Expr(clip, ["", "x 0.5 +"])
-        else:
-            clip = core.std.Expr(clip, ["", "x 0.5 +"])
+        clip = _expr(clip, ["", "x 0.5 +"])
 
         clip_u, clip_v = flexible_inference_with_fallback(
             clips=[clip], network_path=network_path,
@@ -1810,10 +1807,7 @@ def ArtCNN(
 
         clip = core.std.ShufflePlanes([clip, clip_u, clip_v], [0, 0, 0], vs.YUV)
 
-        if clip.format.bits_per_sample == 16:
-            clip = core.akarin.Expr(clip, ["", "x 0.5 -"])
-        else:
-            clip = core.std.Expr(clip, ["", "x 0.5 -"])
+        clip = _expr(clip, ["", "x 0.5 -"])
     else:
         clip = inference_with_fallback(
             clips=[clip], network_path=network_path,
@@ -3083,3 +3077,14 @@ def parse_trt_version(version: int) -> typing.Tuple[int, int, int]:
         return version // 1000, (version // 100) % 10, version % 100
     else:
         return version // 10000, (version // 100) % 100, version % 100
+
+
+def _expr(
+    clip: vs.VideoNode,
+    expr: typing.Union[str, typing.Sequence[str]],
+    format: typing.Optional[int] = None
+) -> vs.VideoNode:
+    try:
+        return core.akarin.Expr(clip, expr, format)
+    except vs.Exception:
+        return core.std.Expr(clip, expr, format)
