23a032449e12816b4778393e5ee328c6e5c16a13
natural-harmonia-gropius <50797982+natural-harmonia-gropius@users.noreply.github.com>
2026-05-08 23:52:31 +0800

    update

--- portable_config/scripts/auto-window-resize-once.lua
+++ portable_config/scripts/auto-window-resize-once.lua
@@ -1,6 +1,9 @@
 local count = 0
 function observer(_, dimensions)
     count = count + 1
+    -- The first, value will be 0x0
+    -- The second, value will be the default dimensions
+    -- The third, value will be the dimensions after resizing
     if count == 3 then
         mp.commandv('set', 'auto-window-resize', 'no')
     end
